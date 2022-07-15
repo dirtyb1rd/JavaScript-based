bc12c2b633d25280d09dbd2be7017bca2d771898
dirtyb1rd <yeskov777.888@gmail.com>
2022-07-15 12:28:40 +0300

    final commit

--- lesson-1.js
+++ lesson-1.js
@@ -1,1 +1,77 @@
 'use strict'
+
+// var a = 1;
+// a = 33;
+// a = null;
+
+//console.log(a);
+
+
+
+
+// let b = 2;
+// b = 22;
+// b = null;
+
+//console.log(b);
+
+
+
+// 
+
+{
+  // var x = 1;
+  // let y = 2;
+
+  //console.log(x);
+  //console.log(y);
+}
+
+//console.log(x);
+//console.log(y);
+
+
+var userName = 'Carl'; // - piority
+var lastName = "Carl";
+var myFriend = `Carl`; // - синтаксис шаблонных строк
+
+// console.log(userName);
+// console.log(lastName);
+// console.log(myFriend);
+
+var user = 'Drake';
+var sample = 'Hello! ' + user;
+var modifySample = `Hello ${user}!`
+
+//console.log(modifySample);
+
+//console.log('Очень большаааааааааааааааааааааааааааааааааааааааааая \n строка');
+//console.log('Очень большаааааааааааааааааааааааааааааааааа\
+//аааааааая строка');
+
+//console.log('He said: "Hello!"');
+//console.log('He said: \'Hello!\'');
+//console.log("He said: 'Hello!'");
+//console.log("He said: \"Hello!\"");
+//console.log(`He said: "Hello!" 'Hello!'`); // - low perfomance
+
+// ЧИСЛА
+
+// var a = 1;
+// var b = 3.121;
+// console.log(a, b);
+
+// console.log(typeof (a));
+// console.log(typeof (b));
+
+
+// console.log(10 * 'Hi');
+
+//console.log(typeof (NaN));
+
+// var i = '3.14';
+// console.log(typeof (i), i);
+// i = -i;
+// console.log(typeof (i), i);
+
+// console.log(parseFloat(i)); // + Number()
